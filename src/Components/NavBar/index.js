a9b9b935fad34ff922b15d0340fdfa85fe35a510
Tom Hinsley <tomhinsley@me.com>
2020-12-16 14:32:42 +0000

    fixed nav bar logo cross browser. Replaced linked svg with local png

--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import './style.css';
+import knowsysLogo from "./knowsysLogo.png";
 
 class NavBar extends React.Component {
 	render() {
 		return (
             <div className="navBar">
                 <div className="navLogoBackground">
-                    <img className="logoImage" src="https://www.svgrepo.com/show/25187/brain.svg" alt="Logo of a brain" />
+                    <img className="logoImage" src={knowsysLogo} alt="Logo of a brain" />
                     <h3>KNOWSY</h3>
                 </div>
                 <button className="myProfileButton">MY PROFILE</button>
