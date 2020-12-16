7a4badb0e75e68d135e9c133bf1324e968395174
Tom Hinsley <tomhinsley@me.com>
2020-12-16 23:14:47 +0000

    added sticky burger menu for mobile devices. working on margin/padding for position sticky

--- src/Components/NavBar/index.js
+++ src/Components/NavBar/index.js
@@ -2,20 +2,24 @@ import React from "react";
 import './style.css';
 import { Route, Link, BrowserRouter as Router, Switch} from 'react-router-dom';
 import knowsysLogo from "./knowsysLogo.png";
+import BurgerMenu from "../BurgerMenu";
+
 
 class NavBar extends React.Component {
 	render() {
 		return (
-            <div className="navBar">
-                <div className="navLogoBackground">
-                    <img className="logoImage" src={knowsysLogo} alt="Logo of a brain" />
-                    <h3>KNOWSY</h3>
-                </div>
+            <div class="navMenu">
+                <BurgerMenu />
+                <div className="navBar">
+                    <div className="navLogoBackground">
+                        <img className="logoImage" src={knowsysLogo} alt="Logo of a brain" />
+                        <h3>KNOWSY</h3>
+                    </div>
                     <Link to ="/addTil" className="newTILButton">ADD TIL</Link>
                     <Link to="/user" className="myProfileButton">MY PROFILE</Link>
                     <Link to ="/login" className="logOutButton">LOG OUT</Link>
                 </div>
-
+            </div>
 		)
 	}
 }
