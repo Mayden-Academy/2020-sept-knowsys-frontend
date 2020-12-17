eb612207f6e7b000ce54d312a9d18cf43d5f06cb
Davin <davin.hayden@outlook.com>
2020-12-17 09:37:21 +0000

    file just has line breakas etc

--- src/Components/CreateAccountForm/index.js
+++ src/Components/CreateAccountForm/index.js
@@ -12,16 +12,18 @@ class CreateAccountForm extends React.Component {
                     <input className="passwordInput"  type="password" placeholder="Password:"></input>
                     <p className="passwordRequirementsHeader">Password must:</p>
                     <p className="passwordRequirementsText">+ Contain at least one upper and Lowercase character</p>
+
                     <div >
                         <label>@</label>
                         <input className="usernameInput" type="text" placeholder="Unique Username:"></input>
                     </div>
                     <p className="usernameRequirements">Max 14 characters</p>
+
                     <label>Add a bio (Optional)</label>
                     <textarea placeholder="Max 500 characters"></textarea>
                     <div class="createAccountButtonContainer">
-                    <button class="cancelFormSubmit">CANCEL</button>
-                    <button url="hello.com" class="confirmFormSubmit" type="submit" text="text">SUBMIT</button>
+                        <button class="cancelFormSubmit">CANCEL</button>
+                        <button url="hello.com" class="confirmFormSubmit" type="submit" text="text">SUBMIT</button>
                     </div>
                 </form>
             </div>
